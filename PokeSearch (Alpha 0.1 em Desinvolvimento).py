903ce9f04ffbd33e550af145bb90ac9b8826fd8c
Gabriel135790 <gabrielcruz20415@gmail.com>
2026-05-26 12:01:49 -0300

    Fix return statement and add missing id key

--- PokeSearch (Alpha 0.1 em Desinvolvimento).py
+++ PokeSearch (Alpha 0.1 em Desinvolvimento).py
@@ -2,7 +2,7 @@ import requests
 import os
 from colorama import Fore, init
 init(autoreset=True)
-os.system('title PokeSearch (Versão Alpha 0.1)'
+os.system('title PokeSearch (Versão Alpha 0.1)')
 
 def procurar(pokemon): # Procura o Nome do Pokemon que você escolheu
     pokemon_format = pokemon.strip().lower()
@@ -11,14 +11,16 @@ def procurar(pokemon): # Procura o Nome do Pokemon que você escolheu
         dados = resposta.json()
         id_ = dados['id']
         experiencia_base = dados['base_experience']
-        altura = dados['height'] + 'cm'
+        altura = dados['height']
         habilidade = [habilidade['ability']['name'] for habilidade in dados['abilities']]
 
         return {
+            'id': id,
             'habilidades': habilidades,
             'altura': altura,
             'experiencia_base': experiencia_base,
             'habilidade': habilidade
+        }
     else:
         print(Fore.RED + f'Erro! Código de Status é {resposta.status_code}, Por Favor Contacte o Desinvolvedor!')
 
